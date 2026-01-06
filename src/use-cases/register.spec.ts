39e0907638064cacae8a16307026e2685d3a6c77
gabrieldasneves <galvesdasneves@gmail.com>
2026-01-06 21:57:50 +0900

    test: implement unit test for user registration use case, verifying password hashing

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,35 @@
-import { test, expect } from "vitest";
+import { describe, test, it, expect } from "vitest";
+import { RegisterUseCase } from "./register.js";
+import { compare } from "bcryptjs";
 
-test("check if it works", () => {
-  expect(true).toBe(true);
+describe("register use case", () => {
+  it("should be hashed the user password upon registration", async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null;
+      },
+      async create(data) {
+        return {
+          id: "user-1",
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      },
+    });
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "123456",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      user.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
 });
